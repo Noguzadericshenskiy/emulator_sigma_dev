81f90def1a634d2d02c89e8168d2557ed4f55823
Гридин Роман <grg@sigma-is.ru>
2024-09-05 21:47:27 +0300

    fix path

--- src/app.py
+++ src/app.py
@@ -1,18 +1,23 @@
 import sys
+import os
 
 from PySide6.QtWidgets import QApplication
-
+from PySide6.QtGui import QIcon
 from src.root_window import MainWindow
 
 
+basedir = os.path.dirname(__file__)
+
+
 def include_style(app):
-    with open("ui/style.qss", "r") as f:
+    with open(os.path.join(basedir, "ui/style.qss"), "r") as f:
         _style = f.read()
         app.setStyleSheet(_style)
 
 
 if __name__ == "__main__":
     app = QApplication([])
+    app.setWindowIcon(QIcon(os.path.join(basedir, "icons", "logo.ico")))
     widget = MainWindow()
     include_style(app)
     widget.show()
